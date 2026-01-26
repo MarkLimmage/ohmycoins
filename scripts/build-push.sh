5d62aa2dde486c621ab9e3399b2a3a6767933e8e
Mark Limmage <marklimmage@gmail.com>
2026-01-26 16:42:41 +1100

    feat(cicd): Implement Staging Deployment Pipeline

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -3,8 +3,13 @@
 # Exit in case of error
 set -e
 
+echo "Building images with TAG=${TAG}..."
+
 TAG=${TAG?Variable not set} \
 FRONTEND_ENV=${FRONTEND_ENV-production} \
 sh ./scripts/build.sh
 
+echo "Pushing images..."
 docker compose -f docker-compose.yml push
+
+echo "Build and push completed."
