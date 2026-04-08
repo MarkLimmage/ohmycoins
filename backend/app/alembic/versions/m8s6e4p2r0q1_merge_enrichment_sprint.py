6fb8be9b88a88033f432f10e059a2f4c54479ad8
Mark Limmage <marklimmage@gmail.com>
2026-04-09 00:48:08 +1000

    chore(alembic): merge migration for reddit deep collector + enrichment generalization

---
+++ backend/app/alembic/versions/m8s6e4p2r0q1_merge_enrichment_sprint.py
@@ -0,0 +1,21 @@
+"""Merge reddit deep collector and enrichment generalization branches
+
+Revision ID: m8s6e4p2r0q1
+Revises: r7e2d1c4f9b3, a7b8c9d0e1f2
+Create Date: 2026-04-09 12:00:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'm8s6e4p2r0q1'
+down_revision = ('r7e2d1c4f9b3', 'a7b8c9d0e1f2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
